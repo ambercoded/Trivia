d74611db25a10bd7065e66ec3f3ec04ceadb9e62
Adrian <mailforadrian@me.com>
2021-03-14 19:23:41 +0100

    Initial Commit

---
+++ TriviaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TriviaApp
+//
+//  Created by Adrian on 14.03.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
